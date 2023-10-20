92eba101546f710ddd0e1996ee5ed41ba1ad3e05
anuThumula <125729713+anuThumula@users.noreply.github.com>
2023-10-20 07:25:07 -0500

    Add files via upload

---
+++ shuffle an array values in java.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+ class ShuffleArray {
+    public static void main(String[] args) {
+        int[] originalArray = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(originalArray);
+        for (int value : originalArray) {
+            System.out.print(value + " ");
+        }
+    }
+
+
+    private static void shuffleArray(int[] array) {
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
+    }
